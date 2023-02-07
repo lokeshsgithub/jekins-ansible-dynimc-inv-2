a58fa696a7012c54a2747a4ef5176c87326e65fe
Lokesh <lokeshreddy05690@gmail.com>
2023-02-07 23:27:02 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,7 @@ region = "ap-south-1"
 }
 
 
-resource "aws_instance" "AWSInstance"{
+resource "aws_instance" "AWSInstance" {
          ami = "ami-0f9d9a251c1a44858"
 	 instance_type = "t2.micro"
 	 security_groups = ["launch-wizard-23"]
