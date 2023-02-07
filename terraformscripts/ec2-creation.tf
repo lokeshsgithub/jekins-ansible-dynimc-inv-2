12ddc7a8329a19e604fd63381003e42b794bdc53
Lokesh <lokeshreddy05690@gmail.com>
2023-02-07 23:24:29 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -4,7 +4,7 @@ region = "ap-south-1"
 
 
 resource "aws_instance" "AWSInstance"{
-     ami = "ami-0f9d9a251c1a44858"
+         ami = "ami-0f9d9a251c1a44858"
 	 instance_type = "t2.micro"
 	 security_groups = ["launch-wizard-23"]
 	 key_name = "lokeshsoftwaresolutions"
